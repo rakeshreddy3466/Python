3ec756b245cd24ecb4fa99e488868bd30b762ff1
RAKESH REDDY KARRI <168624367+rakeshreddy3466@users.noreply.github.com>
2024-11-11 19:11:23 +0100

    Update create_password_genrator.py

--- create_password_genrator.py
+++ create_password_genrator.py
@@ -9,6 +9,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+
 password = ""
 for i in range(0, nr_letters):
     a = random.choice(letters)
