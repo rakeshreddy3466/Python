431caf69068191cee7f4384e720264fa083d26c2
RAKESH REDDY KARRI <168624367+rakeshreddy3466@users.noreply.github.com>
2024-11-19 15:29:37 +0100

    Add files via upload

---
+++ encode_and_decode.py
@@ -0,0 +1,41 @@
+import art
+print(art.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(original_text,shift_amount):
+    cipher_text = ""
+    for x in original_text:
+        if direction == "encode":  
+            if x in alphabet:  
+                a = alphabet.index(x) + shift_amount
+                cipher_text += alphabet[a%26]
+            else:
+                cipher_text += x     
+        # print(cipher_text)
+        else:
+            if x in alphabet:
+                a = alphabet.index(x) - shift_amount
+                cipher_text += alphabet[a%26]
+            else:
+                cipher_text += x
+    print(cipher_text)   
+
+repeat_again = True
+while repeat_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text,shift)
+    
+
+    repeat  = input("if you want run again type 'YES' or type 'NO'").lower()
+    if repeat == "no":
+        repeat_again = False
+        print("good bye")         
+
+
+
+
+
+
